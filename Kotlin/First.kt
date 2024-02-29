a3bcef10d1ac0278d0d1fbdd2078e589738e87b1
LegendUD <161740055+LegendUD@users.noreply.github.com>
2024-02-29 16:35:26 +0000

    Add files via upload

---
+++ Kotlin/First.kt
@@ -0,0 +1,11 @@
+// fun main() {
+//     println("Hello World")
+// }
+// fun main(args : Array<String>) {
+//     println("Hello World")
+//   }
+fun main() {
+    println("Hello World!")
+    println("I am learning Kotlin.")
+    println("It is awesome!")
+  }
